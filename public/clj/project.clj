b71247f47419416802a397f48b9449147d2f0386
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-07-16 15:32:53 +0200

    Add schema as a dependency.

--- public/clj/project.clj
+++ public/clj/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
+                 [prismatic/schema "0.4.3"]
                  [reagent "0.2.0"]]
   :plugins [[lein-cljsbuild "1.0.1"]]
   :cljsbuild {:builds [{:source-paths ["src-cljs"]
